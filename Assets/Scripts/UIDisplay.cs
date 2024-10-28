16ce36f2ec5176bce7f68258986d8dbda58fc280
MuslimGojo <165978580+MuslimGojo@users.noreply.github.com>
2024-10-28 16:32:41 -0700

    Fixed some slight bugs
    This is Gabe using samir's PC
    
    Fixed some slight bugs I noticed
    
    Co-Authored-By: GeoMancerCoding <160548211+GeoMancerCoding@users.noreply.github.com>

--- Assets/Scripts/UIDisplay.cs
+++ Assets/Scripts/UIDisplay.cs
@@ -1,19 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
 
 public class UIDisplay : MonoBehaviour
 {
-
     public Slider slider;
-
+    public PlayerInflicts UIPI;
     // Start is called before the first frame update
     void Start()
     {
-        slider.maxValue = PlayerInflicts.PlayerCurrentHealth;
+        slider.maxValue = UIPI.PlayerMaxHealth;
         slider.value = PlayerInflicts.PlayerCurrentHealth;
     }
 
