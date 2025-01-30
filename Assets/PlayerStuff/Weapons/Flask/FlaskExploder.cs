22ef2ed9de4d1b7871d19f51890a3909236b8d18
Kevin Pittman <kevinthe3rd_2005@hotmail.com>
2025-01-30 13:30:24 -0500

    URP update (spawn of the underworld)
    -implemented URP
    -added throwing flask VFX, script
    -lighting broke dont blame me blame urp

--- Assets/PlayerStuff/Weapons/Flask/FlaskExploder.cs
+++ Assets/PlayerStuff/Weapons/Flask/FlaskExploder.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class FlaskExploder : MonoBehaviour
 {
+    public GameObject HealCast;
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("LevelGeom"))
         {
+            Instantiate(HealCast, transform.position + Vector3.up * 0.25f, Quaternion.identity);
             Destroy(gameObject);
-            Debug.Log("bruh");
         }
     }
 }
