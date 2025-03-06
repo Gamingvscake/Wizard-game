756d024e0ebc7540c77061abca04cbaa9d708d42
GeoMancerCoding <Gabrielerik.m@gmail.com>
2025-03-06 13:43:55 -0500

    added cost showing P1

--- Assets/PlayerStuff/Weapons/InteractableVariables.cs
+++ Assets/PlayerStuff/Weapons/InteractableVariables.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InteractableVariables : MonoBehaviour
@@ -10,4 +11,9 @@ public class InteractableVariables : MonoBehaviour
     public bool SpeedUp;
     public bool StaminaUp;
     public bool ManaUp;
+    public TMP_Text text;
+    private void Start()
+    {
+        if (text != null) text.text = Cost.ToString();
+    }
 }
